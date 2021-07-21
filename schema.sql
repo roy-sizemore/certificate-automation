e43ff407eacf2aa72e588e68369c613937efe262
Roy Sizemore <rsizemore@swampcastle.home>
2021-07-21 14:34:28 -0400

    Add db

--- schema.sql
+++ schema.sql
@@ -1,13 +1,1 @@
 create database if not exists manual_import_db;
-
-create table if not exists attendees (
-    id int not null auto_increment primary key,
-    first_name varchar(255) not null,
-    last_name varchar(255) not null,
-    email varchar(255) not null,
-    title varchar(255)
-);
-
-load data local infile './attendees.csv' into table attendees fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' ignore 1 rows;
-
-select * from attendees;
