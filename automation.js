346c558e56c3dcec92ffbe9aaf1a2b83991ea8ae
Roy Sizemore <rsizemore@swampcastle.home>
2021-07-21 14:33:53 -0400

    Add nodemailer functionality

--- automation.js
+++ automation.js
@@ -1,1 +1,36 @@
+const nodemailer = require('nodemailer');
+
+// Nodemailer function - sends email
+"use strict";
+
+async function email() {
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Kathryn Shipley',
+        to: recipient,
+        subject: 'Your event certificate from ODHE',
+        text: 'Please find attached your event certificate',
+        html: '<p>Please find attached your event certificate</p>',
+        attachments: [{
+            filename: 'certificate.pdf',
+        }]
+    });
+};
+
+email();
+
+export default email;
+
+// Manual import function - pulls attendees from .csv, .xlsx and .xls file(s) and adds them to the database
+
 
